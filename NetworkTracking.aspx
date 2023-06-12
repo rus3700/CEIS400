f1931b358c88d065f4ec8d0370fbe1e1b08c1da9
rus3700 <76888102+rus3700@users.noreply.github.com>
2023-06-12 03:59:29 +0000

    Add files via upload

---
+++ NetworkTracking.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeBehind="NetworkTracking.aspx.cs" Inherits="InventoryTrackingApp.NetworkTracking" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        .form-container {
+            width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        .form-container label {
+            display: block;
+            margin-bottom: 5px;
+        }
+
+        .form-container input[type="text"] {
+            width: 100%;
+            padding: 5px;
+            margin-bottom: 10px;
+        }
+
+        .form-container input[type="submit"] {
+            padding: 5px 10px;
+            background-color: #4CAF50;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        #device-info {
+            display: none;
+            margin-top: 20px;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        #device-info h2 {
+            margin-top: 0;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="form-container">
+            <asp:Label ID="lblAssetTag" runat="server" Text="Asset Tag:"></asp:Label>
+            <asp:TextBox ID="txtAssetTag" runat="server"></asp:TextBox>
+            <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" />
+        </div>
+        <div id="Div1" runat="server" visible="false">
+            <h2>Device Information</h2>
+            <p>Device Name: <asp:Label ID="lblDeviceName" runat="server" Text=""></asp:Label></p>
+            <p>Checked Out To: <asp:Label ID="lblCheckedOutTo" runat="server" Text=""></asp:Label></p>
+            <p>Recent Locations:</p>
+            <asp:GridView ID="gvRecentLocations" runat="server" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="Location" HeaderText="Location" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
+
---
+++ NetworkTracking.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class YourPage : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+        {
+            string assetTag = txtAssetTag.Text;
+
+            // Perform the database operation and retrieve the device information
+            string deviceName = GetDeviceName(assetTag);
+            string checkedOutTo = GetCheckedOutTo(assetTag);
+            DataTable recentLocations = GetRecentLocations(assetTag);
+
+            // Update the UI with the retrieved information
+            lblDeviceName.Text = deviceName;
+            lblCheckedOutTo.Text = checkedOutTo;
+            gvRecentLocations.DataSource = recentLocations;
+            gvRecentLocations.DataBind();
+
+            // Show the device information section
+            deviceInfo.Visible = true;
+        }
+    }
+
+    protected string GetDeviceName(string assetTag)
+    {
+        
+        // Perform the necessary database query to retrieve the device name based on the asset tag
+        // Replace the following placeholder code with your actual database query implementation
+        string deviceName = string.Empty;
+        using (SqlConnection connection = new SqlConnection("Your_Connection_String"))
+        {
+            string query = "SELECT DeviceName FROM Devices WHERE AssetTag = @AssetTag";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@AssetTag", assetTag);
+                connection.Open();
+                deviceName = Convert.ToString(command.ExecuteScalar());
+                connection.Close();
+            }
+        }
+
+        return deviceName;
+    }
+
+    protected string GetCheckedOutTo(string assetTag)
+    {
+        // Perform the necessary database query to retrieve the checked-out information based on the asset tag
+        // Replace the following placeholder code with your actual database query implementation
+        string checkedOutTo = string.Empty;
+        using (SqlConnection connection = new SqlConnection("Your_Connection_String"))
+        {
+            string query = "SELECT CheckedOutTo FROM Checkouts WHERE AssetTag = @AssetTag";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@AssetTag", assetTag);
+                connection.Open();
+                checkedOutTo = Convert.ToString(command.ExecuteScalar());
+                connection.Close();
+            }
+        }
+
+        return checkedOutTo;
+    }
+
+    protected DataTable GetRecentLocations(string assetTag)
+    {
+        // Perform the necessary database query to retrieve the recent locations based on the asset tag
+        // Replace the following placeholder code with your actual database query implementation
+        DataTable recentLocations = new DataTable();
+        using (SqlConnection connection = new SqlConnection("Your_Connection_String"))
+        {
+            string query = "SELECT Location FROM Locations WHERE AssetTag = @AssetTag ORDER BY Date DESC";
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@AssetTag", assetTag);
+                connection.Open();
+                using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                {
+                    adapter.Fill(recentLocations);
+                }
+                connection.Close();
+            }
+        }
+
+        return recentLocations;
+    }
+}
